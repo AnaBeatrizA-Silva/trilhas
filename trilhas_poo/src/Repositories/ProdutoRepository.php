37c1fff575e6ff30086bfb547b744ae6672ed33a
Ana Beatriz <ana.beatriz.adriano.silva.1929@gmail.com>
2026-07-22 14:59:09 -0300

    Setimo exercicio concluido

---
+++ trilhas_poo/src/Repositories/ProdutoRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace App\Repositories;
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+class ProdutoRepository {
+
+    public function salvar():void {
+        echo "Produto cadastrado \n";
+    }
+}
